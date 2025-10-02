1959c1a4b1c995470f1292e2ad376460b2f7d406
Rubendvb <rubenvasquez556@gmail.com>
2025-10-02 03:53:23 -0300

    creating tests login failed

--- cypress/e2e/login-api.cy.js
+++ cypress/e2e/login-api.cy.js
@@ -14,4 +14,42 @@ describe('testes em API', () => {
       cy.get('@token').should('exist');
     });
   });
+
+  context('Requisições de usuário clínica em especialistas', () => {
+    beforeEach(() => {
+      cy.fixture('especialistas.json').as('especialistas');
+      cy.loginApi(Cypress.env('email'), Cypress.env('senha'));
+    });
+
+    it('POSt em especialista', () => {
+      cy.get('@especialistas').then((dados) => {
+        const especialista = dados.especialistas[0];
+
+        cy.request({
+          method: 'POST',
+          url: Cypress.env('api_clinica'),
+          body: {
+            nome: especialista.nome,
+            email: especialista.email,
+            senha: especialista.senha,
+            endereco: {
+              cep: especialista.cep,
+              rua: especialista.rua,
+              numero: especialista.numero,
+              complemento: especialista.complemento,
+              estado: especialista.estado,
+            },
+          },
+        }).then((response) => {
+          if (response.status !== 201) {
+            cy.log(`O status ${response.status} não é o padrão 201`);
+          }
+
+          expect(response.body).to.have.property('id');
+          expect(response.body).to.have.property('nome');
+          expect(response.body).to.have.property('email');
+        });
+      });
+    });
+  });
 });
