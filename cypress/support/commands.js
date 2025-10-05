9eeb2cba04148e9e6e0ccdcb2759ee7c77537823
Rubendvb <rubenvasquez556@gmail.com>
2025-10-05 20:13:02 -0300

    Refactor login command to separate visibility check and click action

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,8 @@ Cypress.Commands.add('login', (email, senha) => {
     cy.visit('/login');
     cy.get('[data-test="inputLoginEmail"]').type(email);
     cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false });
-    cy.get('[data-test="botaoTeste"]').should('be.visible').click();
+    cy.get('[data-test="botaoTeste"]').should('be.visible');
+    cy.get('[data-test="botaoTeste"]').click();
     cy.location('pathname').should('eq', '/dashboard');
   });
 });
